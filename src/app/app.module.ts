e022f32264ca07737d4c4b9d65c0deada40aeb99
Mio86 <miodrag.maric86@gmail.com>
2018-02-12 11:36:09 +0100

    ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
